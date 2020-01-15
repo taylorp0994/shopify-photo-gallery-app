e9ffa335ae3a2e91263fb9ea847006c16e878c54
Patrick Taylor <taylorp0994@gmail.com>
2020-01-15 06:54:43 -0600

    update pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import App from 'next/app';
 import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
 
 class MyApp extends App {
   render() {
@@ -10,7 +13,9 @@ class MyApp extends App {
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
+        <AppProvider i18n={translations}>
           <Component {...pageProps} />
+        </AppProvider>
       </React.Fragment>
     );
   }
