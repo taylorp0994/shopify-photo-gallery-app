2c75ffbbd053cbcb1d0b087f876cfeb43ae9b379
Patrick Taylor <taylorp0994@gmail.com>
2020-01-15 07:11:49 -0600

    add toggle to annotated-layout

--- pages/annotated-layout.js
+++ pages/annotated-layout.js
@@ -5,17 +5,22 @@ import {
   FormLayout,
   Layout,
   Page,
+  SettingToggle,
   Stack,
   TextField,
+  TextStyle,
 } from '@shopify/polaris';
 
 class AnnotatedLayout extends React.Component {
   state = {
     discount: '10%',
+    enabled: false,
   };
 
   render() {
-    const { discount } = this.state;
+    const { discount, enabled } = this.state;
+    const contentStatus = enabled ? 'Disable' : 'Enable';
+    const textStatus = enabled ? 'enabled' : 'disabled';
 
     return (
       <Page>
@@ -42,6 +47,21 @@ class AnnotatedLayout extends React.Component {
               </Form>
             </Card>
           </Layout.AnnotatedSection>
+          <Layout.AnnotatedSection
+            title="Price updates"
+            description="Temporarily disable all Sample App price updates"
+          >
+            <SettingToggle
+              action={{
+                content: contentStatus,
+                onAction: this.handleToggle,
+              }}
+              enabled={enabled}
+            >
+              This setting is{' '}
+              <TextStyle variation="strong">{textStatus}</TextStyle>.
+            </SettingToggle>
+          </Layout.AnnotatedSection>
         </Layout>
       </Page>
     );
@@ -56,6 +76,12 @@ class AnnotatedLayout extends React.Component {
 
   handleChange = (field) => {
     return (value) => this.setState({ [field]: value });
+  };
+
+  handleToggle = () => {
+    this.setState(({ enabled }) => {
+      return { enabled: !enabled };
+    });
   };
 }
 
