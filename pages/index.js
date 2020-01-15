60aac3ae7ba8fc55ceae68c0a2fa3e520836345a
Patrick Taylor <taylorp0994@gmail.com>
2020-01-15 07:00:50 -0600

    more polaris updates

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
-import { TextStyle } from '@shopify/polaris';
+import { Layout, Page, TextStyle } from '@shopify/polaris';
 
 const Index = () => (
-  <div>
-    <TextStyle variation="positive">
-      Sample app using React and Next.js
-    </TextStyle>
-  </div>
+  <Page>
+    <Layout>
+      <TextStyle variation="positive">
+        Sample app using React and Next.js
+      </TextStyle>
+    </Layout>
+  </Page>
 );
 
 export default Index;
