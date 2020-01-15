e1d921367ce323957dd28844c4ed0640e09f98dd
Patrick Taylor <taylorp0994@gmail.com>
2020-01-15 07:07:46 -0600

    more polaris updates for emptystate

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import { Layout, Page, TextStyle } from '@shopify/polaris';
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
-  <Page>
-    <Layout>
-      <TextStyle variation="positive">
-        Sample app using React and Next.js
-      </TextStyle>
-    </Layout>
-  </Page>
+  <EmptyState
+    heading="Discount your products temporarily"
+    action={{
+      content: 'Select products',
+      onAction: () => console.log('clicked'),
+    }}
+    image={img}
+  >
+    <p>Select products to change their price temporarily.</p>
+  </EmptyState>
 );
 
 export default Index;
